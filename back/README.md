40a16d857559ce1d52b8a13a4b3b695995474698
Paul Breuil <tykapl.breuil@gmail.com>
2024-03-25 21:15:36 +0100

    switch to proper pocketbase migrations

--- back/README.md
+++ back/README.md
@@ -8,3 +8,11 @@ First install a **recent** version of `go` then run the commands :
 go install
 air serve # air is a utility to give hot reloading to the go code
 ```
+
+## Register changes to the database
+
+To register new changes to the database schema, simply run :
+
+```bash
+go run main.go migrate collections
+```
--- back/main.go
+++ back/main.go
@@ -3,9 +3,14 @@ package main
 import (
 	"log"
 	"os"
+	"strings"
+
 	"github.com/pocketbase/pocketbase"
 	"github.com/pocketbase/pocketbase/apis"
 	"github.com/pocketbase/pocketbase/core"
+	"github.com/pocketbase/pocketbase/plugins/migratecmd"
+
+	_ "back/migrations"
 )
 
 func main() {
@@ -15,6 +20,15 @@ func main() {
 	app.OnBeforeServe().Add(func(e *core.ServeEvent) error {
 		e.Router.GET("/*", apis.StaticDirectoryHandler(os.DirFS("./pb_public"), false))
 		return nil
+	})
+
+	// loosely check if it was executed using "go run"
+	isGoRun := strings.HasPrefix(os.Args[0], os.TempDir())
+
+	migratecmd.MustRegister(app, app.RootCmd, migratecmd.Config{
+		// enable auto creation of migration files when making collection changes in the Admin UI
+		// (the isGoRun check is to enable it only during development)
+		Automigrate: isGoRun,
 	})
 
 	if err := app.Start(); err != nil {
