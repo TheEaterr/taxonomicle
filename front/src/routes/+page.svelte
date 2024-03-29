3a297f2642430199def985cce963f2c9127c205d
Paul Breuil <tykapl.breuil@gmail.com>
2024-03-29 11:02:04 +0100

    fix vscode config

--- front/src/routes/+page.svelte
+++ front/src/routes/+page.svelte
@@ -8,28 +8,26 @@
 		<div class="hero-content p-10 text-center sm:p-14 lg:p-20">
 			<div class="max-w-md">
 				<h1 class="title text-5xl font-bold text-primary sm:text-6xl lg:text-7xl">Taxonomicle</h1>
-				<div class="m-5">
+				<div class="m-3 mt-5">
 					<button
 						on:click={() => goto('/game/daily')}
-						class="btn btn-primary bg-primary-content text-lg font-bold text-neutral hover:text-primary-content"
+						class="btn btn-primary btn-lg bg-primary-content text-lg font-bold text-neutral hover:text-primary-content"
 						>Daily</button
 					>
 					<button
 						on:click={() => goto('/game/endless')}
-						class="btn btn-secondary ml-5 bg-secondary-content text-lg font-bold text-neutral hover:text-secondary-content"
+						class="btn btn-secondary btn-lg ml-5 bg-secondary-content text-lg font-bold text-neutral hover:text-secondary-content"
 						>Endless</button
 					>
 				</div>
-				<div class="m-5">
+				<div>
 					<button
-						on:click={() => goto('/game/tutorial')}
-						class="btn btn-neutral bg-neutral-content font-bold text-neutral hover:text-neutral-content"
-						>How to play</button
+						on:click={() => goto('/tutorial')}
+						class="btn btn-outline btn-neutral btn-sm font-bold text-neutral">How to play</button
 					>
 					<button
-						on:click={() => goto('/game/endless')}
-						class="btn btn-neutral ml-5 bg-neutral-content font-bold text-neutral hover:text-neutral-content"
-						>Endless</button
+						on:click={() => goto('/about')}
+						class="btn btn-outline btn-neutral btn-sm ml-1 font-bold text-neutral">About</button
 					>
 				</div>
 			</div>
