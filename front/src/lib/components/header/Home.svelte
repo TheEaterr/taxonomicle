0510edb2fee5231fa1b41fac68cbe53287e42a49
Paul Breuil <tykapl.breuil@gmail.com>
2024-03-29 20:25:30 +0100

    pretty animations for header buttons

--- front/src/lib/components/header/Home.svelte
+++ front/src/lib/components/header/Home.svelte
@@ -1,11 +1,25 @@
 <script lang="ts">
 	import { goto } from '$app/navigation';
 	import { IconHome } from '@tabler/icons-svelte';
+
+	let button: HTMLButtonElement;
+	const animate = () => {
+		button.animate(
+			[{ transform: 'scale(1)' }, { transform: 'scale(1.2)' }, { transform: 'scale(1)' }],
+			{
+				duration: 500,
+				iterations: Infinity,
+				easing: 'ease-in-out'
+			}
+		);
+	};
 </script>
 
 <button
+	bind:this={button}
 	on:click={() => {
-		goto('/');
+		animate();
+		goto('/game/endless');
 	}}
 >
 	<IconHome class="swap-on h-10 w-10" />
--- front/src/lib/components/header/Return.svelte
+++ front/src/lib/components/header/Return.svelte
@@ -1,9 +1,27 @@
 <script lang="ts">
 	import { IconArrowLeft } from '@tabler/icons-svelte';
+
+	let button: HTMLButtonElement;
+	const animate = () => {
+		button.animate(
+			[
+				{ transform: 'translate(0px)' },
+				{ transform: 'translate(-5px)' },
+				{ transform: 'translate(0px)' }
+			],
+			{
+				duration: 500,
+				iterations: Infinity,
+				easing: 'ease-in-out'
+			}
+		);
+	};
 </script>
 
 <button
+	bind:this={button}
 	on:click={() => {
+		animate();
 		window.history.back();
 	}}
 >
