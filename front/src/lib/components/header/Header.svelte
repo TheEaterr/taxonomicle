f2117bb58bdd9bcefd91f4665f0bda7bd50aaf9f
Paul Breuil <tykapl.breuil@gmail.com>
2024-03-30 14:26:56 +0100

    fix nav header

--- front/src/lib/components/header/Header.svelte
+++ front/src/lib/components/header/Header.svelte
@@ -15,22 +15,19 @@
 	export let reset: (() => void) | undefined;
 </script>
 
-<div class="navbar sticky top-0 z-10">
-	<div class="flex-1">
-		<div class="rounded-xl bg-base-200 bg-opacity-80 p-2">
-			<Return />
-			<Home />
-			{#if reset}
-				<ResetButton {reset} />
-			{/if}
-		</div>
-	</div>
-	<div class="flex-none gap-2 rounded-xl bg-base-200 p-2">
-		<label class="swap swap-rotate">
-			<!-- this hidden checkbox controls the state -->
-			<input type="checkbox" data-toggle-theme="dark,light" data-act-class="ACTIVECLASS" />
-			<IconSun class="swap-on h-10 w-10" stroke={2} />
-			<IconMoon class="swap-off h-10 w-10" stroke={2} />
-		</label>
-	</div>
+<div class="fixed left-0 top-0 z-10 m-2 rounded-xl bg-base-200 bg-opacity-80 p-2">
+	<Return />
+	<Home />
+	{#if reset}
+		<ResetButton {reset} />
+	{/if}
 </div>
+<div class="fixed right-0 top-0 z-10 m-2 rounded-xl bg-base-200 bg-opacity-80 p-2">
+	<label class="swap swap-rotate">
+		<!-- this hidden checkbox controls the state -->
+		<input type="checkbox" data-toggle-theme="dark,light" data-act-class="ACTIVECLASS" />
+		<IconSun class="swap-on h-10 w-10" stroke={2} />
+		<IconMoon class="swap-off h-10 w-10" stroke={2} />
+	</label>
+</div>
+<div class="mb-[72px] w-full"></div>
