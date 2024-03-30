8b9efbb1f2e4caa1955b3bdd45ac4cedc03b274b
Paul Breuil <tykapl.breuil@gmail.com>
2024-03-30 14:33:03 +0100

    start working pretty button

--- front/src/lib/components/Taxon/TaxonButton.svelte
+++ front/src/lib/components/Taxon/TaxonButton.svelte
@@ -5,9 +5,12 @@
 <div>
 	{#each taxons as taxon}
 		<button
-			class="bg-{taxon} border-{taxon}-content btn text-{taxon}-content hover:bg-{taxon}-content hover:text-{taxon} hover:border-{taxon}"
+			class="bg-{taxon} border-{taxon}-content btn text-{taxon}-content hover:bg-{taxon}-content hover:text-{taxon} hover:border-{taxon} relative"
 		>
-			{taxon}
+			This is a test
+			<div class="absolute bottom-0 right-0 bg-{taxon}">
+				{taxon}
+			</div>
 		</button>
 	{/each}
 </div>
