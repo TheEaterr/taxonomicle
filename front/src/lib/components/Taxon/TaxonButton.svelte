1da672cbca80be1913bfbac2e50c6f9ec72610f2
Paul Breuil <tykapl.breuil@gmail.com>
2024-03-30 12:54:03 +0100

    use much better automatic color management

--- front/src/lib/components/Taxon/TaxonButton.svelte
+++ front/src/lib/components/Taxon/TaxonButton.svelte
@@ -1,37 +1,15 @@
 <script lang="ts">
+	import taxons from '../../taxons.js';
 </script>
 
 <div>
-	<button class="btn border-kingdom-content bg-kingdom">kingdom</button>
-	<button class="btn border-subkingdom-content bg-subkingdom">subkingdom</button>
-	<button class="btn border-infrakingdom-content bg-infrakingdom">infrakingdom</button>
-	<button class="btn border-superphylum-content bg-superphylum">superphylum</button>
-	<button class="btn border-phylum-content bg-phylum">phylum</button>
-	<button class="btn border-subphylum-content bg-subphylum">subphylum</button>
-	<button class="btn border-infraphylum-content bg-infraphylum">infraphylum</button>
-	<button
-		class="btn border-superclass-content bg-superclass
-		">superclass</button
-	>
-	<button class="btn border-class-content bg-class">class</button>
-	<button class="btn border-subclass-content bg-subclass">subclass</button>
-	<button
-		class="btn border-infraclass-content bg-infraclass
-		">infraclass</button
-	>
-	<button class="btn border-superorder-content bg-superorder">superorder</button>
-	<button class="btn border-order-content bg-order">order</button>
-	<button class="btn border-suborder-content bg-suborder">suborder</button>
-	<button class="btn border-infraorder-content bg-infraorder">infraorder</button>
-	<button class="btn border-pavorder-content bg-pavorder">pavorder</button>
-	<button class="btn border-superfamily-content bg-superfamily">superfamily</button>
-	<button class="btn border-family-content bg-family">family</button>
-	<button class="btn border-subfamily-content bg-subfamily">subfamily</button>
-	<button class="btn border-tribe-content bg-tribe">tribe</button>
-	<button class="btn border-subtribe-content bg-subtribe">subtribe</button>
-	<button class="btn border-genus-content bg-genus">genus</button>
-	<button class="btn border-subgenus-content bg-subgenus">subgenus</button>
-	<button class="btn border-species-content bg-species">species</button>
+	{#each taxons as taxon}
+		<button
+			class="bg-{taxon} border-{taxon}-content btn text-{taxon}-content hover:bg-{taxon}-content hover:text-{taxon} hover:border-{taxon}"
+		>
+			{taxon}
+		</button>
+	{/each}
 </div>
 
 <style>
