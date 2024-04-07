028eb1fc1b36dd35cbc9c7d9ceff521a9133544f
Paul Breuil <tykapl.breuil@gmail.com>
2024-04-07 22:53:19 +0200

    add suspense to loading between pages

--- front/src/routes/+layout.svelte
+++ front/src/routes/+layout.svelte
@@ -21,7 +21,7 @@
 	{#if $navigating}
 		<!-- TODO: make pretty loading icon after making branding items -->
 		<div class="flex h-screen w-screen items-center justify-center">
-			<span class="loading loading-spinner w-20 text-neutral"></span>
+			<span class="animate-suspense loading loading-spinner w-20 text-neutral opacity-0"></span>
 		</div>
 	{:else}
 		<slot />
@@ -32,4 +32,25 @@
 </QueryClientProvider>
 
 <style>
+	@keyframes fade-in {
+		0% {
+			opacity: 0;
+		}
+		100% {
+			opacity: 1;
+		}
+	}
+	@keyframes suspense {
+		0% {
+			opacity: 1;
+		}
+		100% {
+			opacity: 1;
+		}
+	}
+
+	.animate-suspense {
+		animation: suspense 5s cubic-bezier(0.4, 0, 0.6, 1) infinite;
+		animation-delay: 300ms;
+	}
 </style>
