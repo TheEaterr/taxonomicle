2787cbc55441aecf12cc21ea678ff85bfeff5258
Paul Breuil <tykapl.breuil@gmail.com>
2024-04-08 22:33:38 +0200

    add custom taxon route and page

---
+++ front/src/routes/game/custom/[id]/+page.server.ts
@@ -0,0 +1,11 @@
+import { getGoalTaxonData, getTaxonData } from '$lib/pocketBase';
+import type { PageServerLoad } from './$types';
+
+export const load: PageServerLoad = async ({ params }) => {
+	const goalTaxon = await getGoalTaxonData(params.id);
+	const animaliaTaxon = await getTaxonData('Q729___________', goalTaxon.taxon.path);
+	return {
+		goalTaxon,
+		animaliaTaxon
+	};
+};
