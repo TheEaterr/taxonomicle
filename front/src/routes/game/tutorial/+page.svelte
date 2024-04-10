bcb812d71b0b1f5e90c1f9fe08f6c226ad58b5af
Paul Breuil <tykapl.breuil@gmail.com>
2024-04-10 21:49:14 +0200

    add initial paragraph

--- front/src/routes/game/tutorial/+page.svelte
+++ front/src/routes/game/tutorial/+page.svelte
@@ -16,16 +16,26 @@
 	<div class="modal-box w-11/12 max-w-5xl bg-base-200">
 		<h3 class="small-title text-3xl font-bold text-primary">Welcome to Taxonomicle!</h3>
 		<p class="py-4 text-justify">
-			Press ESC key or click the button below to close Press ESC key or click the button below to
-			closeb Press ESC key or click the button below to closeb Press ESC key or click the button
-			below to closeb Press ESC key or click the button below to closeb Press ESC key or click the
-			button below to closeb Press ESC key or click the button below to closeb Press ESC key or
-			click the button below to closeb Press ESC key or click the button below to closeb Press ESC
-			key or click the button below to closeb Press ESC key or click the button below to closeb
-			Press ESC key or click the button below to closeb Press ESC key or click the button below to
-			closeb Press ESC key or click the button below to closeb Press ESC key or click the button
-			below to closeb Press ESC key or click the button below to closeb Press ESC key or click the
-			button below to closeb Press ESC key or click the button below to closeb
+			Taxonomicle is a game where you try to find a species by descending the tree of life.
+			<br />
+			For this, Taxonomicle uses the field of biology called taxonomy, the scientific study of naming,
+			defining and classifying groups of biological organisms based on shared characteristics. These
+			groups are called taxa (singular: taxon) and are given a rank to build a hierarchy. For Taxonomicle,
+			we start at the kingdom animalia, i.e. the group of all animals. Below there are, amidst others,
+			phylum, class, order, family, genus, and species.
+			<br />
+			For example, Vertebrates are a subphylum (i.e. something smaller than a phylum but bigger than
+			a class) that contain, among others, the class of mammals, the class of birds, the class of reptiles,
+			and the class of amphibians. We humans are in the family Hominidae (the great apes) and in the
+			order of the primates.
+			<br />
+			In Taxonomicle, you will be given a random species and you have try to find its place in the tree.
+			For this, you progressively descend the tree by choosing between the children of the currently
+			selected taxon.
+			<br />
+			In this tutorial, you will be guided through an example of a game using a predefined species. The
+			tutorial will give you tips and tricks on how to play the game, as taxonomy can be quite confusing
+			at first.
 		</p>
 		<div class="modal-action">
 			<form method="dialog">
